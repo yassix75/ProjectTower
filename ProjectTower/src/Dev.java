0cc80681c6b330f83782f7690f8013ae655ae68b
Thaws <Thaws@LAPTOP-1S387MH7>
2020-05-09 13:04:07 +0200

    devmap

--- ProjectTower/src/Dev.java
+++ ProjectTower/src/Dev.java
@@ -4,6 +4,6 @@ public class Dev {
 		System.out.println("Hello world!!!!");
 		System.out.println("CECI EST LE DEV!!!");
 		System.out.println("le dev aura des sous branches pour chaque fonctionalite du jeu");
-		System.out.println("CECI EST LE DEVMAP");
+		System.out.println("CECI EST LE DEVMAP sous branche de dev");
 	}
 }
